26a8d735b5bc23eb5626109b42bd8ff7398b3a3f
Arsene Vlad <50497935+Arsuh@users.noreply.github.com>
2020-04-24 17:38:53 +0300

    Update helper.py
    Added momentum and clipnorm option

--- helper.py
+++ helper.py
@@ -53,7 +53,7 @@ def create_model(hparams):
     dec = Decoder(hparams['VOCAB'], hparams['BATCH_SIZE'], hparams['EMBEDDING'],
                   hparams['RNN1'], hparams['RNN2'], hparams['RNN_TYPE'], hparams['DROPOUT_DEC'])
     #opt = tf.keras.optimizers.Adam(learning_rate=hparams['LR'])
-    opt = tf.keras.optimizers.SGD(learning_rate=hparams['LR'], momentum=0.3)
+    opt = tf.keras.optimizers.SGD(learning_rate=hparams['LR'], momentum=hparams['MOMENTUM'], clipnorm=hparams['CLPINORM'])
     return enc, dec, opt
 
 def create_dataset(v, batch_size, buffer_size):
